22ee3d1d589c8670d56c78792a5db33ea8048e76
kweather <taeyoung.yang@kweather.co.kr>
2026-02-06 17:24:21 +0900

    [feat] Mod redis cache

--- infra/src/main/java/com/example/infra/redis/RedisConfig.java
+++ infra/src/main/java/com/example/infra/redis/RedisConfig.java
@@ -7,15 +7,15 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RedisConfig {
 
     private final ObjectMapper objectMapper;
 
-    public RedisConfig(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
+    public RedisConfig() {
+        this.objectMapper = new ObjectMapper();
     }
 
     @Primary
